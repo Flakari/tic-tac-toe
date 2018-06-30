aad4e18291f01cc40c4ea8534f3dea1d883d638b
Nate Dimock <natedimock@yahoo.com>
2018-06-30 15:07:43 -0500

    Game now logs array when winning move is made, no more moves can be made, game reset hasn't been made yet

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,7 @@ console.log(player2.symbol);
 
 const gameBoard = (() => {
     const board = [];
+    let gamePlay = true;
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -22,7 +23,7 @@ const gameBoard = (() => {
         [0, 4, 8],
         [2, 4, 6]
     ];
-    return {board, winConditions};
+    return {board, gamePlay, winConditions};
 })();
 
 const displayController = (() => {
@@ -30,16 +31,18 @@ const displayController = (() => {
     const boardSpaces = Array.from(document.querySelector('#game-board').children);
     boardSpaces.forEach(space => {
         space.addEventListener('click', e => {
-            if (e.target.textContent == '') {
+            if (e.target.textContent == '' && gameBoard.gamePlay == true) {
                 if (symbol == player1.symbol) {
                     e.target.textContent = player1.symbol;
                     player1.spaces.push(boardSpaces.indexOf(e.target));
                     console.log('Player 1 Spaces: ' + player1.spaces);
+                    winCheck(player1.spaces, boardSpaces);
                     symbol = player2.symbol;
                 } else {
                     e.target.textContent = player2.symbol;
                     player2.spaces.push(boardSpaces.indexOf(e.target));
                     console.log('Player 2 Spaces: ' + player2.spaces);
+                    winCheck(player2.spaces, boardSpaces);
                     symbol = player1.symbol;
                 }
             } else {
@@ -51,3 +54,30 @@ const displayController = (() => {
         });
     });
 })();
+
+function winCheck(playerSpaces, boardSpaces) {
+    let arrayCheck = [];
+    for(let i = 0; i < gameBoard.winConditions.length; i++) {
+        for(let j = 0; j < gameBoard.winConditions[i].length; j++) {
+            if (playerSpaces.includes(gameBoard.winConditions[i][j]) === true) {
+                arrayCheck.push('true');
+            } else {
+                arrayCheck.push('false');
+            }
+            if (arrayCheck.length == 3) {
+                if (arrayCheck.includes('false') === false) {
+                    console.log(arrayCheck);
+                    gameBoard.gamePlay = false;
+                } else {
+                    arrayCheck = [];
+                }
+            }
+        }
+    }
+}
+
+function gameStop(spaces) {
+    spaces.forEach(space => {
+        space.removeEventListener('click', e => {return});
+    });
+}
